37cb5c172f735cb5f506053ec60cfe75afc1d626
Cibelle Montor <montorcibelle@gmail.com>
2019-12-02 12:16:39 -0700

    stash

--- models/items.js
+++ models/items.js
@@ -1,8 +1,13 @@
 module.exports = function (sequelize, DataTypes) {
   var items = sequelize.define("items", {
     name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    status: DataTypes.STRING
+    status: DataTypes.STRING,
+    orderNumber: DataTypes.INTEGER,
+    mods: DataTypes.STRING,
+    highlight: DataTypes.STRING,
+    averageTime: DataTypes.STRING,
+    currentTime: DataTypes.STRING
+
   });
   return items;
 };
