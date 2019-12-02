f1f900b6b67143f6657cdaf6ff5537e7d6dc8e40
Brian Graf <brian.g.graf@gmail.com>
2019-12-02 13:48:38 -0700

    Added default values

--- models/items.js
+++ models/items.js
@@ -1,13 +1,19 @@
 module.exports = function (sequelize, DataTypes) {
   var items = sequelize.define("items", {
     name: DataTypes.STRING,
-    status: DataTypes.STRING,         // "not started", "working", "finished"
+    status:{  // "not started", "working", "finished"
+      type : DataTypes.STRING,
+      defaultValue : "not started"
+    },
     orderNumber: DataTypes.INTEGER,
     station: DataTypes.INTEGER,      // 1 : Salad, 2 : Dessert
     mods: DataTypes.STRING,
     highlight: DataTypes.STRING,
     averageTime: DataTypes.STRING,
-    currentTime: DataTypes.STRING
+    currentTime:{
+      type : DataTypes.STRING,
+      defaultValue : "0:00"
+    }
   });
   return items;
 };
