868422b040f3a217c4135b7c0d55a0a7708eb135
Brian Graf <brian.g.graf@gmail.com>
2019-12-04 12:21:40 -0700

    Time working for stations

--- public/js/station.js
+++ public/js/station.js
@@ -52,3 +52,47 @@ $cooking.on("dblclick", function () {
 
 
 
+
+
+
+function sendNewTime(id, newTime)
+{
+  $.ajax({
+    url : "/api/items/" + id,
+    type : "PUT",
+    data : {
+      currentTime : newTime
+    }
+  }).then(function (res, err){
+    console.log("Sent time");
+  })
+}
+
+function incrementTime(currentTime)
+{
+  let newTime = currentTime;
+
+  newTime = newTime.split(":");
+
+  let minutes = parseInt(newTime[0]);
+  let seconds = parseInt(newTime[1]);
+
+  if(seconds < 59)
+  {
+    seconds++;
+    if(seconds < 10)
+    {
+      seconds = "0" + seconds;
+    }
+  }
+  else
+  {
+    minutes++;
+    seconds = 0;
+  }
+
+  newTime = `${minutes}:${seconds}`;
+
+  return newTime;
+
+};
