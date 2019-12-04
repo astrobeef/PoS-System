c93120d1231678e21a44646ea693a9402c26c371
Brian Graf <brian.g.graf@gmail.com>
2019-12-04 12:12:32 -0700

    Set up time

--- public/js/expo.js
+++ public/js/expo.js
@@ -49,7 +49,72 @@ $recallOrder.on("click", function () {
     }
   )
 
-})
+});
+
+setInterval(function () {
+
+  $times = $(".current-time");
+
+  let element = 0;
+
+  while($times[element])
+  {
+    const $time = $($times[element]);
+    let timeID = $time.attr("id");
+    timeID.split("-");
+    timeID = parseInt(timeID[timeID.length - 1]);
+    console.log(timeID);
+
+    $time.text(incrementTime($time.text()));
+
+   
+    element++;
+
+    sendNewTime(timeID, $time.text());
+
+  };
 
 
+}, 1000);
 
+function sendNewTime(id, newTime)
+{
+  $.ajax({
+    url : "/api/items/" + id,
+    type : "PUT",
+    data : {
+      currentTime : newTime
+    }
+  }).then(function (res, err){
+    console.log("Sent time");
+  })
+}
+
+function incrementTime(currentTime)
+{
+  let newTime = currentTime;
+
+  newTime = newTime.split(":");
+
+  let minutes = parseInt(newTime[0]);
+  let seconds = parseInt(newTime[1]);
+
+  if(seconds < 59)
+  {
+    seconds++;
+    if(seconds < 10)
+    {
+      seconds = "0" + seconds;
+    }
+  }
+  else
+  {
+    minutes++;
+    seconds = 0;
+  }
+
+  newTime = `${minutes}:${seconds}`;
+
+  return newTime;
+
+};
